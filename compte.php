853cca3372f4afa4b9c67b0423b6b8af8267d74d
Camille Semaan <csemaan@cmaisonneuve.qc.ca>
2021-11-03 11:33:07 -0400

    Séance 18 avant la pause

--- compte.php
+++ compte.php
@@ -4,21 +4,24 @@
 ?>
     <section class="principale">
     <?php if(!isset($_SESSION['uc'])) {  ?>
-        <form action="compte.php" method="post">
-            <fieldset>
-                <legend><?= $frmLegende; ?></legend>
-                <input type="text" name="courriel" placeholder="<?= $frmCourrielPH; ?>">
-                <input type="password" name="mdp" id="mdp" placeholder="<?= $frmMdpPH; ?>">
-                <input type="submit" value="<?= $frmBoutonConnecter; ?>">
-                <?php if(isset($erreurConnexion)) : ?>
-                    <div class="erreur">Erreur de connexion : réessayez!</div>
-                <?php endif; ?>
-            </fieldset>
-            <div class="actions-compte">
-                <a href="#"><?= $lienMdpOublie; ?></a>
-                <a href="#"><?= $lienNouveauCompte; ?></a>
-            </div>
-        </form>
+        <div class="formulaires-utilisateurs">
+            <form action="compte.php" method="post">
+                <fieldset>
+                    <legend><?= $frmLegende; ?></legend>
+                    <input type="text" name="courriel" placeholder="<?= $frmCourrielPH; ?>">
+                    <input type="password" name="mdp" placeholder="<?= $frmMdpPH; ?>">
+                    <input type="submit" value="<?= $frmBoutonConnecter; ?>">
+                    
+                </fieldset>
+                <div class="actions-compte">
+                    <span><?= $lienMdpOublie; ?></span>
+                    <span><?= $lienNouveauCompte; ?></span>
+                </div>
+            </form>
+            <?php if(isset($erreurConnexion)) : ?>
+                <div class="erreur">Erreur de connexion : réessayez!</div>
+            <?php endif; ?>
+        </div>
     <?php } else { ?>
         <h2>Mes investissements</h2>
     <?php } ?>
